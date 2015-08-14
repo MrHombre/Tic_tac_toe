e42b5d6452e5cf00186550fb4a8a737cc4661cd3
James <jlett89@gmail.comssh-keygen -t rsa -C jlett89@gmail.com>
2015-08-14 11:38:55 -0700

    First commit for ttt. My first gem

---
+++ spec/ttt_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Ttt do
+  it 'has a version number' do
+    expect(Ttt::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
